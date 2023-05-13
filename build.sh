35fddc2970b10f5fc891a83f7f54868d19ed6f04
the <w180112@gmail.com>
2023-05-13 14:05:20 +0800

    fix compile error

--- build.sh
+++ build.sh
@@ -12,8 +12,8 @@ pushd lib/picohttpparser
 gcc -fPIC -c -o picohttpparser.o picohttpparser.c
 ar rcs libpicohttpparser.a picohttpparser.o
 gcc -shared -o libpicohttpparser.so picohttpparser.o
-sudo cp libpicohttpparser.so /usr/local/lib64/libpicohttpparser.so
-sudo cp libpicohttpparser.a /usr/local/lib64/libpicohttpparser.a
+sudo cp libpicohttpparser.so /usr/local/lib/libpicohttpparser.so
+sudo cp libpicohttpparser.a /usr/local/lib/libpicohttpparser.a
 sudo cp picohttpparser.h /usr/local/include/picohttpparser.h
 popd
 
